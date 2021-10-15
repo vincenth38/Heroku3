b99f45a97c04878423e0582680d4e4883a8a5ae4
vincenth38 <vincent.heloin@gmail.com>
2021-10-14 20:47:10 -0700

    Initial commit

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+#app.title=tabtitle
 
 app.layout = html.Div([
     dcc.Upload(
